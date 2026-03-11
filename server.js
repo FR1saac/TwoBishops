1a5d37d8fdf63df7b238dbce448d6c3c420664e9
FR1saac <shalevcam@gmail.com>
2026-03-12 00:20:50 +0200

    fixing port

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const app = express()
 app.use(express.static("public"))
 app.use(express.json())
 
-const server = app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000")
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 const wss = new WebSocket.Server({ server })
 
